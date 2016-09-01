9e58acf0a55502e0b09d69a1359813fff1ad443b
gssakib <gssakib>
2016-08-31 23:40:25 -0400

    started hw1

---
+++ JAVA_Homeworks/src/Schedule.java
@@ -0,0 +1,15 @@
+
+public class Schedule {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Gazi Sakib's Schedule");
+		System.out.println("SBU ID: 109849940");
+		System.out.println("                     Monday    Tuesday    Wednesday    Thursday    Friday");
+		System.out.println("8:30 am - 9:20 am");
+		System.out.println("8:30 am - 9:20 am");
+		// TODO Auto-generated method stub
+
+	}
+
+}
