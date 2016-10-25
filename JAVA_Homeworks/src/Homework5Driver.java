28c11bd271a1cf37edc8bc51c2dd745df0ea883a
gssakib <gssakib>
2016-10-25 18:44:47 -0400

    start of hw6

--- JAVA_Homeworks/src/Homework5Driver.java
+++ JAVA_Homeworks/src/Homework5Driver.java
@@ -44,14 +44,14 @@ public class Homework5Driver {
 		
 		
 		//System.out.println("Part 4:");
-		//int[] nums;
+		int[] nums;
 		//nums = Homework5Methods.interleaveArrays(new int[]{2,2,5,4},new int[]{6,2,1,8,4,4},2,10);
 		//nums = Homework5Methods.interleaveArrays(new int[]{10,9,8},new int[]{1,2,3},5,20);
 		//nums = Homework5Methods.interleaveArrays(new int[]{2,5,1,4}, new int[]{10,5,2},1,8);
 		//nums = Homework5Methods.interleaveArrays(new int[]{2,3,4,5},new int[]{6,3,2,1},2,4);
 		//nums = Homework5Methods.interleaveArrays(new int[]{1,2},new int[]{2,3,4,5},4,-3);
 		
-		//System.out.println(Arrays.toString(nums));
+		System.out.println(Arrays.toString(nums));
 		
 		
 		
